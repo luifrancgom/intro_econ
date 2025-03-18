e8beb42d0881f240e03594eb20e330029648e9ac
luifrancgom <luifrancgom@gmail.com>
2025-03-17 20:14:55 -0500

    Adding word cloud exercise for 2025-1

---
+++ 000_data/000_core_econ_cloud_words/script_cloud_word.R
@@ -0,0 +1,34 @@
+# Libraries ----
+library(wordcloud2)
+library(ggwordcloud)
+
+# Import data ----
+merge_data <- read_csv(file = "000_data/000_core_econ_cloud_words/merge_data/merge_data.csv")
+
+
+# Visualization ----
+merge_data_word_freq_es <- merge_data |> 
+  count(clean_word, sort = TRUE)
+
+merge_data_word_freq_en <- merge_data |> 
+  count(en_word, sort = TRUE)
+
+set.seed(seed = 1234)  
+word_cloud_es <- merge_data_word_freq_es |> 
+  ggplot(aes(label = clean_word, size = n)) + 
+  geom_text_wordcloud(aes(color = n)) +
+  scale_size_area(max_size = 15) +
+  scale_color_gradient(low = "#2c3e50", 
+                       high = "#e31a1c")
+
+word_cloud_es
+
+set.seed(seed = 1234)
+word_cloud_en <- merge_data_word_freq_en |> 
+  ggplot(aes(label = en_word, size = n)) + 
+  geom_text_wordcloud(aes(color = n)) +
+  scale_size_area(max_size = 15) +
+  scale_color_gradient(low = "#2c3e50", 
+                       high = "#e31a1c")
+
+word_cloud_en
---
+++ 000_data/000_core_econ_cloud_words/script_merge_data.R
@@ -0,0 +1,18 @@
+# Libraries ----
+library(tidyverse)
+library(fs)
+
+# List directore
+list_files <- dir_ls(path = "000_data/000_core_econ_cloud_words/clean_data/", 
+                     regexp = r'(\.csv$)')
+
+# Import data ----
+merge_data <- list_files |> 
+  map(.f = \(x) read_csv(file = x)) |> 
+  bind_rows() |> 
+  mutate(id = row_number(), .before = course)
+
+# Export data 
+merge_data |> 
+  write_csv(file = "000_data/000_core_econ_cloud_words/merge_data/merge_data.csv")
+  
